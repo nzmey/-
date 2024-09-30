f0261386cf5be154da26a4d5b7be49e300afda99
nzmey <nzmey@yandex.ru>
2024-10-01 02:10:26 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,7 @@
+neme = 'Андрей'
+age = 58
+is_student = True
+print('Имя : ' , neme)
+print('Возраст : ' , age)
+print('Новый возраст : ' , age + 1)
+print('Является ли студентом : ' , is_student)
---
+++ module_1_T_2.py
@@ -0,0 +1,5 @@
+task_count = 12
+hour_count = 1.5
+name_cour = 'Python'
+time_of_one_task = (hour_count / task_count)
+print('Курс :', name_cour,', затрачено часов :', hour_count,', среднее время выполнения :', time_of_one_task,'часа.')
