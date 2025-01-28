664cd4520205a163be14e097710be06af5a94219
nzmey <nzmey@yandex.ru>
2025-01-28 15:36:30 +0300

    Update module_10_5.py

--- module_10_5.py
+++ module_10_5.py
@@ -2,41 +2,46 @@ print('\n')
 
 '''   <<<  Многопроцессорное считывание.  >>>   '''
 
-import multiprocessing, time, datetime
+import multiprocessing, time, datetime, os
 
-file_names_line = [f'./file {number}.txt' for number in range(1, 5)]
-file_names_process = [f'./file {number}.txt' for number in range(1, 5)]
-
+file_names_line = [f'file {number}.txt' for number in range(1, 5)]
+file_names_process = [f'file {number}.txt' for number in range(1, 5)]
 
 def read_info_line(name):
     all_data = []
     with open(name, "r", encoding = "UTF-8") as fl:
-        for _ in fl:
+        ps = os.path.getsize(name)
+        print(f'Размер файла <{name}> для Line функции = {ps}.')
+        for _ in range(ps):
             s = fl.readline()
-            # print(s)
-            if s == '':
-                break
-            else:
-                all_data.append(s.strip())
-            # time.sleep(0.5)
-    # print(all_data)
-    return all_data
+            all_data.append(s.strip())
+            # if s == '':
+            #     break
+            # else:
+            #     all_data.append(s.strip())
+    print(f'Line функция: Файл <{name}>:')
+    for i in range(5):
+        print(all_data[i])
+    # return all_data
 
 # read_info_line("file 1.txt")
 
 def read_info_process(name):
     all_data = []
     with open(name, "r", encoding = "UTF-8") as fl:
-        for _ in fl:
+        ps = os.path.getsize(name)
+        print(f'Размер файла <{name}> для Process функции = {ps}.')
+        for _ in range(ps):
             s = fl.readline()
-            # print(s)
-            if s == '':
-                break
-            else:
-                all_data.append(s.strip())
-            # time.sleep(0.5)
-    # print(all_data)
-    return all_data
+            all_data.append(s.strip())
+            # if s == '':
+            #     break
+            # else:
+            #     all_data.append(s.strip())
+    print(f'Process функция: Файл <{name}>:')
+    for i in range(5):
+        print(all_data[i])
+    # return all_data
 
 
 if __name__ == '__main__':
@@ -44,52 +49,50 @@ if __name__ == '__main__':
     line_list = []
 
     begin_line = datetime.datetime.now()
-    line_list += list(map(read_info_line, file_names_line))[0]
+    line_list += map(read_info_line, file_names_line)
     end_line = datetime.datetime.now()
 
-    for i in range(10):
-        print(line_list[i])
+    # for i in range(5):
+    #     print(line_list[i])
     print(f'Время выполнения ЛИНЕЙНОГО считывания = {end_line - begin_line}.')
 
-    process_list = []
+    # process_list = []
 
     begin_process = datetime.datetime.now()
     '''Создание пула из процессов - число которых будет определять функция "cpu_count()".'''
     with multiprocessing.Pool(processes = multiprocessing.cpu_count()) as pl:
-        res = pl.map(read_info_process, file_names_process)
-        process_list += list(res)[0]
+    # with multiprocessing.Pool(processes = 4) as pl:
+        res_p = pl.map(read_info_process, file_names_process)
     end_process = datetime.datetime.now()
 
+    # begin_process = datetime.datetime.now()
+    # for i in file_names_process:
+    #     p_i = multiprocessing.Process(target = read_info_process, args = (i,))
+    #     p_i.start()
+    #     # p_i.join()
+    # end_process = datetime.datetime.now()
 
-    for i in range(10):
-        print(process_list[i])
-
+    # for i in range(5):
+    #     print(res[0][i])
     print(f'Время МУЛЬТИПРОЦЕССОРНОГО считывания = {end_process - begin_process}.')
 
 
 '''
 PS.
-Результаты следующие:
-Время выполнения ЛИНЕЙНОГО считывания = 0:00:08.199469.
-Время МУЛЬТИПРОЦЕССОРНОГО считывания = 0:00:10.683611.
-Или - около того.
+После запуска программы - результаты следующие:
+Время выполнения ЛИНЕЙНОГО считывания = 0:25:46.602943.
+Время МУЛЬТИПРОЦЕССОРНОГО считывания = 0:10:52.065537.
 
-В моём компе - всего ДВА физических ядра и ЧЕТЫРЕ виртуальных.
-Очевидно - именно поэтому - ЛИНЕЙНОЕ считывание 
-происходит быстрее МУЛЬТИПРОЦЕССОРНОГО считывания.
+После того, как я установил для обеих функций 
+выход из цикла считывания файла по пустой строке - 
+- я получил следующие результаты:
+Время выполнения ЛИНЕЙНОГО считывания = 0:00:14.024424.
+Время МУЛЬТИПРОЦЕССОРНОГО считывания = 0:00:06.208811.
+
 Число процессов - я отдал на откуп функции "cpu_count()"
  - ей виднее сколько процессов запускать.
 
-Функции считывают файлы - намного быстрее, чем предполагалось в задании.
-Там линейная версия считывает файлы аж за целых ТРИ с лишним минуты.
-У меня тоже такое было, - пока я не запретил считывание пустых строк.
-
-При всём при этом - вынужден был обеспечить возврат списка 
-в каждой из считывающих функций. Иначе - не работает.
 '''
-
-
-
 
 
 
